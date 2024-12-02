832ecda1b65cc74e72af4fa6051693393e0693f6
Liciaav <liciaavasc@gmail.com>
2024-12-02 12:47:25 -0500

    update package.json, connection.ts and resolvers.ts

--- server/src/schema/resolvers.ts
+++ server/src/schema/resolvers.ts
@@ -1,27 +1,5 @@
-import mongoose, { Document, Model } from 'mongoose';
 import { signToken, AuthenticationError } from '../services/auth.js';
-
-// Define the Book interface
-interface Book {
-  bookId: string;
-  authors: string[];
-  description: string;
-  title: string;
-  image: string;
-  link: string;
-}
-
-// Define the User interface extending Mongoose's Document
-interface UserDocument extends Document {
-  username: string;
-  email: string;
-  password: string;
-  savedBooks: Book[];
-  isCorrectPassword(password: string): Promise<boolean>;
-}
-
-// Ensure Mongoose model with types
-const User: Model<UserDocument> = mongoose.model<UserDocument>('User');
+import User from '../models/User.js'
 
 export const resolvers = {
   Query: {
@@ -38,12 +16,12 @@ export const resolvers = {
       if (!user || !(await user.isCorrectPassword(password))) {
         throw new AuthenticationError('Incorrect credentials');
       }
-      const token = signToken({ username: user.username, email: user.email, _id: user._id });
+      const token = signToken(user.username, user.email, user._id );
       return { token, user };
     },
     addUser: async (_: any, { username, email, password }: { username: string; email: string; password: string }) => {
       const user = await User.create({ username, email, password });
-      const token = signToken({ username: user.username, email: user.email, _id: user._id});
+      const token = signToken(user.username, user.email, user._id);
       return { token, user };
     },
     saveBook: async (_: any, { input }: { input: Book }, context: any) => {
